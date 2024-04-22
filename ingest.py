580d3940c005a1a5109b149e9d0452e842e2bf42
omarMohamedo-o <omarmo.abdelsalam@domain.com>
2024-04-22 15:38:01 +0200

    initial commit

---
+++ ingest.py
@@ -0,0 +1,162 @@
+import logging
+import os
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+
+from langchain.docstore.document import Document
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+from chromadb.config import Settings
+
+from langchain_community.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredFileLoader, UnstructuredMarkdownLoader
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Define the folder for storing database
+SOURCE_DIRECTORY = "SOURCE_DOCUMENTS/"
+
+PERSIST_DIRECTORY = "DB/"
+
+# Can be changed to a specific number
+INGEST_THREADS = os.cpu_count() or 8
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+    anonymized_telemetry=False,
+    is_persistent=True,
+)
+DOCUMENT_MAP = {
+    ".txt": TextLoader,
+    ".md": UnstructuredMarkdownLoader,
+    ".py": TextLoader,
+    # ".pdf": PDFMinerLoader,
+    ".pdf": UnstructuredFileLoader,
+    ".csv": CSVLoader,
+    ".xls": UnstructuredExcelLoader,
+    ".xlsx": UnstructuredExcelLoader,
+    ".docx": Docx2txtLoader,
+    ".doc": Docx2txtLoader,
+}
+
+def file_log(logentry):
+   file1 = open("file_ingest.log","a")
+   file1.write(logentry + "\n")
+   file1.close()
+   print(logentry + "\n")
+
+def load_single_document(file_path: str) -> Document:
+    # Loads a single document from a file path
+    try:
+       file_extension = os.path.splitext(file_path)[1]
+       loader_class = DOCUMENT_MAP.get(file_extension)
+       if loader_class:
+           file_log(file_path + ' loaded.')
+           loader = loader_class(file_path)
+       else:
+           file_log(file_path + ' document type is undefined.')
+           raise ValueError("Document type is undefined")
+       return loader.load()[0]
+    except Exception as ex:
+       file_log('%s loading error: \n%s' % (file_path, ex))
+       return None
+
+def load_document_batch(filepaths):
+    logging.info("Loading document batch")
+    # create a thread pool
+    with ThreadPoolExecutor(len(filepaths)) as exe:
+        # load files
+        futures = [exe.submit(load_single_document, name) for name in filepaths]
+        # collect data
+        if futures is None:
+           file_log(name + ' failed to submit')
+           return None
+        else:
+           data_list = [future.result() for future in futures]
+           # return data and file paths
+           return (data_list, filepaths)
+
+
+def load_documents(source_dir: str) -> list[Document]:
+    # Loads all documents from the source documents directory, including nested folders
+    paths = []
+    for root, _, files in os.walk(source_dir):
+        for file_name in files:
+            print('Importing: ' + file_name)
+            file_extension = os.path.splitext(file_name)[1]
+            source_file_path = os.path.join(root, file_name)
+            if file_extension in DOCUMENT_MAP.keys():
+                paths.append(source_file_path)
+
+    # Have at least one worker and at most INGEST_THREADS workers
+    n_workers = min(INGEST_THREADS, max(len(paths), 1))
+    chunksize = round(len(paths) / n_workers)
+    docs = []
+    with ProcessPoolExecutor(n_workers) as executor:
+        futures = []
+        # split the load operations into chunks
+        for i in range(0, len(paths), chunksize):
+            # select a chunk of filenames
+            filepaths = paths[i : (i + chunksize)]
+            # submit the task
+            try:
+               future = executor.submit(load_document_batch, filepaths)
+            except Exception as ex:
+               file_log('executor task failed: %s' % (ex))
+               future = None
+            if future is not None:
+               futures.append(future)
+        # process all results
+        for future in as_completed(futures):
+            # open the file and load the data
+            try:
+                contents, _ = future.result()
+                docs.extend(contents)
+            except Exception as ex:
+                file_log('Exception: %s' % (ex))
+
+    return docs
+
+
+def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
+    # Splits documents for correct Text Splitter
+    text_docs, python_docs = [], []
+    for doc in documents:
+        if doc is not None:
+           file_extension = os.path.splitext(doc.metadata["source"])[1]
+           if file_extension == ".py":
+               python_docs.append(doc)
+           else:
+               text_docs.append(doc)
+    return text_docs, python_docs
+
+def main():
+    # Load documents and split in chunks
+    logging.info(f"Loading documents from {SOURCE_DIRECTORY}")
+    documents = load_documents(SOURCE_DIRECTORY)
+    text_documents, python_documents = split_documents(documents)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    python_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.PYTHON, chunk_size=880, chunk_overlap=200
+    )
+    texts = text_splitter.split_documents(text_documents)
+    texts.extend(python_splitter.split_documents(python_documents))
+    logging.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+    logging.info(f"Split into {len(texts)} chunks of text")
+    
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    db = Chroma.from_documents(
+        texts,
+        embeddings,
+        persist_directory=PERSIST_DIRECTORY,
+        client_settings=CHROMA_SETTINGS,
+    )
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
+    )
+    main()
